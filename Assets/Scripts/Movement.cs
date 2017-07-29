84729b27028d1885398f781973751daa9b486d6b
Facundo Atrio <peakend22@gmail.com>
2017-07-29 06:56:22 -0300

    First Version

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float movementSpeed;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        transform.position = transform.position + new Vector3 (-1.0f, 0.0f, 0.0f) * movementSpeed * Time.deltaTime;
+	}
+}
