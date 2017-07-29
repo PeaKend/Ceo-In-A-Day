86277b4816d1362da3977d14b0faad84405d4566
Facundo Atrio <peakend22@gmail.com>
2017-07-29 09:43:03 -0300

    Added a size randomizer

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,6 +6,9 @@ public class MapGenerator : MonoBehaviour {
     public float spawnTime;
     public float minRandom;
     public float maxRandom;
+    public float objectSize;
+    public float minrandomSize;
+    public float maxrandomSize;
     public GameObject spawnPrefab;
     Transform spawnPosition;
 
@@ -22,18 +25,29 @@ public class MapGenerator : MonoBehaviour {
 		
 	}
     
+    // Methods
+
     float timeRandomizer ()
     {
         spawnTime = Random.Range(minRandom, maxRandom);
         return spawnTime;
     }
 
+    float sizeRandomizer ()
+    {
+        objectSize = Random.Range(minrandomSize, maxrandomSize);
+        return objectSize;
+    }
+
     IEnumerator spawnObjects()
     {
+        GameObject instantiatedObject;
         while (true)
         {
             timeRandomizer();
-            GameObject.Instantiate(spawnPrefab, spawnPosition.position, Quaternion.identity);
+            sizeRandomizer();
+            instantiatedObject = GameObject.Instantiate(spawnPrefab, spawnPosition.position, Quaternion.identity);
+            instantiatedObject.transform.localScale = new Vector3(objectSize, instantiatedObject.transform.localScale.y, 1.0f);
             yield return new WaitForSeconds(spawnTime);
         }
     }
