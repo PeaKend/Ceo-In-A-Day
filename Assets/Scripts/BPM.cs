4a7101e076734485a51b63aa985029dc8b9e372b
Facundo Atrio <peakend22@gmail.com>
2017-07-29 11:02:14 -0300

    Added text placeholder and BPM script

--- Assets/Scripts/BPM.cs
+++ Assets/Scripts/BPM.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class BPM : MonoBehaviour {
     public float bpm;
+    BPMText bpmtextGameobject;
+    
+    private void Awake()
+    {
+        bpmtextGameobject = GameObject.FindGameObjectWithTag("BPMCounter").GetComponent<BPMText>();
+        addBpm(1.0f);
+    }
 
     public void addBpm(float amountofBpm)
     {
         bpm += amountofBpm;
+        bpmtextGameobject.updateText(bpm);
     }
 }
---
+++ Assets/Scripts/BPMText.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BPMText : MonoBehaviour {
+	
+    public void updateText(float bpm)
+    {
+        gameObject.GetComponent<Text>().text = bpm.ToString();
+    }
+}
