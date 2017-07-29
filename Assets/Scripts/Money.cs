d71be52003aa8fa544de142d2517c7f958d79379
Facundo Atrio <peakend22@gmail.com>
2017-07-29 15:29:42 -0300

    Money drop and money script added

---
+++ Assets/Scripts/Money.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour {
+
+    void Awake()
+    {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2 (Random.Range(-10.0f, 10.0f),
+                                                                     Random.Range(0.0f, 10.0f)),
+                                                        ForceMode2D.Impulse);
+        StartCoroutine(destroyMoney());
+    }
+
+    IEnumerator destroyMoney()
+    {
+        yield return new WaitForSeconds(3.0f);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/MoneyDrop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyDrop : MonoBehaviour {
+    public float dropRate;
+    public GameObject money;
+
+    void Awake()
+    {
+        StartCoroutine(dropMoney());
+    }
+
+    IEnumerator dropMoney()
+    {
+        while (true)
+        {
+            Instantiate(money, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(1.0f / dropRate);
+        }
+    }
+}
