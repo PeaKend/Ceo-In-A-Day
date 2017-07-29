07e4067822d85f56f33d3914aeb5a6c9343d8582
Facundo Atrio <peakend22@gmail.com>
2017-07-29 08:10:16 -0300

    Jump added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+    public float jumpForce;
+    Rigidbody2D playerRB;
 
-	void Start () {
+    void Awake()
+    {
+        playerRB = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Start ()
+    {
 		
 	}
 	
 	void Update () {
         if (Input.GetButtonDown("Jump"))
         {
+            // Debug
             Debug.Log("Jump");
+
+            // Code
+            playerRB.AddForce(Vector2.up * jumpForce * Time.deltaTime, ForceMode2D.Impulse);
         }
         if (Input.GetButtonDown("Crouch"))
         {
