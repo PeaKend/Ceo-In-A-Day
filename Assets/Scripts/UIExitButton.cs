f80bea1f85247621cf78d71739ac87f40d2de874
Facundo Atrio <peakend22@gmail.com>
2017-07-29 16:12:42 -0300

    Added a main menu placeholder

---
+++ Assets/Scripts/UIExitButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIExitButton : MonoBehaviour {
+    Button btn;
+
+    void Awake()
+    {
+        btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(taskonClick);
+    }
+
+    public void taskonClick()
+    {
+        Application.Quit();
+    }
+
+
+}
---
+++ Assets/Scripts/UIPlayButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIPlayButton : MonoBehaviour {
+    Button btn;
+
+    void Awake()
+    {
+        btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(taskonClick);
+    }
+
+    public void taskonClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
