728eb221649e175cd93404c99e7c72bd5c9d6e92
Facundo Atrio <peakend22@gmail.com>
2017-07-30 12:39:32 -0300

    Added more assets

--- Assets/Scripts/Money.cs
+++ Assets/Scripts/Money.cs
@@ -6,10 +6,15 @@ public class Money : MonoBehaviour {
 
     void Awake()
     {
-        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2 (Random.Range(-10.0f, 10.0f),
-                                                                     Random.Range(0.0f, 10.0f)),
-                                                        ForceMode2D.Impulse);
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2 (Random.Range(0.0f, 15.0f),
+                                                                     Random.Range(0.0f, 15.0f)), ForceMode2D.Impulse);
         StartCoroutine(destroyMoney());
+    }
+
+    void FixedUpdate()
+    {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-25.0f,
+                                                                    0.0f), ForceMode2D.Force);
     }
 
     IEnumerator destroyMoney()
--- Assets/Scripts/PrefabRandomizer.cs
+++ Assets/Scripts/PrefabRandomizer.cs
@@ -3,9 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PrefabRandomizer : MonoBehaviour {
+    public bool RandomRotation;
+    public float MaxRotationAmount;
+
+    public bool RandomScale;
+    public float RandomScaleX;
+    public float RandomScaleY;
 
     void Awake()
-    {
-            
+    {     
+            if (RandomRotation)
+        {
+            transform.eulerAngles = new Vector3(0.0f, 
+                                                0.0f,
+                                                Random.Range(-MaxRotationAmount, MaxRotationAmount));
+        }
+            if (RandomScale)
+        {
+            if (RandomScaleX == 0.0f)
+            {
+                RandomScaleX = transform.localScale.x;
+            }
+            if (RandomScaleY == 0.0f)
+            {
+                RandomScaleY = transform.localScale.y;
+            }
+            transform.localScale = new Vector3(Random.Range(transform.localScale.x, RandomScaleX), 
+                                               Random.Range(transform.localScale.y, RandomScaleY), 
+                                               0.0f);
+        }
     }
+    
 }
